f9074d32dcb84464d0a970bd47ed37525a12e79d
wng-mchl <maxmicwong@gmail.com>
2026-04-22 15:08:29 +0800

    added pgs conn, created table and updated the database routing

--- server.js
+++ server.js
@@ -1,35 +1,65 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
-const APP_NAME = process.env.APP_NAME || "Finance API"; 
+const APP_NAME = process.env.APP_NAME || "Finance API";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-let transactions = []; // temporary storage
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS transactions (
+      id SERIAL PRIMARY KEY,
+      name TEXT,
+      amount FLOAT,
+      type TEXT,
+      date TEXT
+    );
+  `);
+};
+
+initDB();
+
 
 app.get('/health', (req, res) => {
   res.send(`${APP_NAME} up. Server live.`)
 })
 
 // GET all transactions
-app.get("/transactions", (req, res) => {
-  res.json(transactions);
+app.get("/transactions", async (req, res) => {
+  const result = await pool.query("SELECT * FROM transactions");
+  res.json(result.rows);
 });
 
 // POST new transaction
-app.post("/transactions", (req, res) => {
-  const newTransaction = req.body;
-  transactions.push(newTransaction);
-  res.json(newTransaction);
+app.post("/transactions", async (req, res) => {
+  const { name, amount, type, date } = req.body;
+
+  const result = await pool.query(
+    "INSERT INTO transactions (name, amount, type, date) VALUES ($1, $2, $3, $4) RETURNING *",
+    [name, amount, type, date]
+  );
+
+  res.json(result.rows[0]);
 });
 
 // DELETE transaction
-app.delete("/transactions/:id", (req, res) => {
+app.delete("/transactions/:id", async (req, res) => {
   const id = parseInt(req.params.id);
-  transactions = transactions.filter(t => t.id !== id);
+
+  await pool.query("DELETE FROM transactions WHERE id = $1", [id]);
+
   res.json({ success: true });
 });
 
